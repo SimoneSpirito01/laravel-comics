c4b1cb7ab86df9219be736f9b4abf7a238079cc2
SimoneSpirito <simospiri01@gmail.com>
2022-02-02 17:35:17 +0100

    main with blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,28 @@
 @endsection
 
 @section('content')
-    
+    <main>
+        <div class="content">
+            <div class="container">
+                <div class="title">
+                    <span>Current Series</span>
+                </div>
+                <div class="series">
+                    @foreach ($comics as $serie)
+                        <div class="serie">
+                            <div class="image">
+                                <img src="{{$serie['thumb']}}" alt="{{$serie['title']}}">
+                            </div>
+                            <div class="text">
+                                <span>{{$serie['title']}}</span>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="load">
+                    <span>Load More</span>
+                </div>
+            </div>
+        </div>
+    </main>
 @endsection
