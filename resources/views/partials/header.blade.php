e616f10b4a2c090f26aac7e8ae4c794c0cf9ee25
SimoneSpirito <simospiri01@gmail.com>
2022-02-02 17:18:16 +0100

    header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    <div class="blue-bar">
+        <div class="container">
+            <span>dc power&#8480;visa&reg;</span>
+            <span>additional dc sites<span class="triangle">&#9660;</span></span>
+        </div>
+    </div>
+    <div class="container">
+          <div class="logo">
+              <img src="{{asset('images/dc-logo.png')}}" alt="logo">
+          </div>
+          <nav>
+              <ul>
+                  <li><a href="#">Characters</a></li>
+                  <li><a class="active" href="#">Comics</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">Tv</a></li>
+                  <li><a href="#">Games</a></li>
+                  <li><a href="#">Collectibles</a></li>
+                  <li><a href="#">Videos</a></li>
+                  <li><a href="#">Fans</a></li>
+                  <li><a href="#">News</a></li>
+                  <li><a href="#">Shop</a></li>
+              </ul>
+          </nav>
+    </div>
+    <div class="hero"></div>
+</header>
